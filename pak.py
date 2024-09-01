a954d008e2bdae8134a8458de10ba8762112899b
rabbix2020 <67027531+rabbix2020@users.noreply.github.com>
2024-09-01 07:41:22 +0200

    Add files via upload

---
+++ pak.py
@@ -0,0 +1,39 @@
+import struct
+
+class pak_header:
+     def __init__(self):
+        self.id = None
+        self.offet = None
+        self.size = None
+     def read(self, data : list):
+        self.id = data[:4].decode("ascii")
+        if not (self.id == "PACK"):	raise TypeError("pak_header.read(): it's not a PAK file")
+        self.offset = struct.unpack("i", data[4:8])[0]
+        self.size = struct.unpack("i", data[8:12])[0]
+
+entry_size = 64
+class pak_entry:
+     def __init__(self):
+        self.name = None
+        self.offset = None
+        self.size = None
+
+class pak:
+     def read_files(self, pak_head : pak_header, data : list):
+        files = []
+        files_count = self.files_count
+        for i in range(files_count):
+           current_entry = pak_entry()
+           current_entry.name = data[pak_head.offset+i*entry_size:pak_head.offset+i*entry_size+56].decode("ascii").replace('\0', '')
+           current_entry.offset = struct.unpack("i", data[pak_head.offset+i*entry_size+56:pak_head.offset+i*entry_size+60])[0]
+           current_entry.size = struct.unpack("i", data[pak_head.offset+i*entry_size+60:pak_head.offset+i*entry_size+64])[0]
+           files.append(current_entry)
+
+        return files
+
+     def __init__(self, data):
+        self.data = data
+        self.header : pak_header = pak_header()
+        self.header.read(data)
+        self.files_count = self.header.size // entry_size
+        self.files : list = self.read_files(self.header, data);
